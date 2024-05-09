bb489a15b7918c2a50f6320034c98d2b1cadaa5b
kiramZodiac <kiramzodiac@gmail.com>
2024-05-09 14:06:28 +0300

    new changes

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,22 +1,23 @@
 import React from "react";
+import Link from "next/link";
 
 function NavBar() {
   return (
-    <div className=" text-blue-700  float-right w-2/6 pt-5  ">
+    <div className=" text-white  float-right w-2/6 pt-5   ">
       <div className="  space-x-5 mr-30  flex  p-1 pr-7 text-2xl fixed">
         <ul className=" hover:font-bold">
-          <a href="">Home</a>
+          <Link href="./">Home</Link>
         </ul>
-        <ul className="  hover: hover:text-blue-600 hover:font-bold">
+        <ul className="  hover: hover:font-bold">
           <a href="">Find a Mechanic</a>
         </ul>
-        <ul className="  hover:text-blue-600 hover:font-bold">
+        <ul className="  hover:font-bold">
           <a href="">Services</a>
         </ul>
-        <ul className=" hover: hover:text-blue-600  hover:font-bold">
-          <a href="">About us</a>
+        <ul className=" hover:  hover:font-bold">
+          <Link href="./about">About us</Link>
         </ul>
-        <ul className=" hover: hover:text-blue-600 hover:font-bold">
+        <ul className=" hover: hover:font-bold">
           <a href="">Contact</a>
         </ul>
       </div>
---
+++ src/app/about/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import NavBar from "../NavBar";
+import Link from "next/link";
+
+function about() {
+  return (
+    <div>
+      <div className=" bg-sky-950 fixed w-full h-16 pb-10">
+        <NavBar />
+      </div>
+      <div className=" text-red-600 pt-10">
+        <h1>ABOUT US</h1>
+      </div>
+    </div>
+  );
+}
+
+export default about;
